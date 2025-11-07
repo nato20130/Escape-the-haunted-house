db905e4a54790aa11c18f7fc619b9e6964c6df64
nato20130 <nato.ramez123@gmail.com>
2025-11-06 21:03:19 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// This file holds any constants that can be modified
+const APP_NAME = "Escape the Haunted House";
+const DEFAULTS = {
+    startingLevel: 1,
+    maxLevels: 5,
+    maxTime: 30,
+};
