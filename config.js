5193c39f0fc0c133611762c9b6790be0aeae3dac
nato20130 <nato.ramez123@gmail.com>
2025-11-13 20:48:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
-// This file holds any constants that can be modified
+// Configuration file for the game
+
 const APP_NAME = "Escape the Haunted House";
+
 const DEFAULTS = {
     startingLevel: 1,
+    startingScore: 0,
     maxLevels: 5,
-    maxTime: 30,
+    maxTime: 30,   // Time per level in seconds
 };
